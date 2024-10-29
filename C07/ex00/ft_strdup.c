c8629eb46383ff123d8be376d0c58835c1c95f3d
Heriniaina <heriniainaramamonjitiana@gmail.com>
2024-10-29 19:51:08 +0100

    eae

---
+++ C07/ex00/ft_strdup.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hramamon <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/21 08:10:31 by hramamon          #+#    #+#             */
+/*   Updated: 2023/12/21 08:30:22 by hramamon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include <stdlib.h>
+int	ft_str_length(char *str)
+{
+	int	index;
+
+	index = 0;
+	while (str[index])
+		index++;
+	return (index);
+}
+
+char	*ft_strdup(char *src)
+{
+	int		index;
+	char	*dest;
+	char	*d;
+
+	index = 0;
+	d = ((dest = (char *)malloc(ft_str_length(src) * sizeof(char) + 1)));
+	if (!d)
+	{
+		return (0);
+	}
+	while (src[index])
+	{
+		dest[index] = src[index];
+		index++;
+	}
+	dest[index] = '\0';
+	return (dest);
+}
---
+++ C07/ex01/ft_range.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hramamon <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/21 08:40:31 by hramamon          #+#    #+#             */
+/*   Updated: 2023/12/21 08:50:22 by hramamon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int	*ft_range(int min, int max)
+{
+	int		range;
+	int		index;
+	int		*buffer;
+	int		*d;
+
+	if (min >= max)
+		return (0);
+	range = max - min;
+	d = (buffer = malloc(range * sizeof(int)));
+	if (!d)
+		return (0);
+	index = 0;
+	while (index < range)
+	{
+		buffer[index] = min + index;
+		index++;
+	}
+	return (buffer);
+}
